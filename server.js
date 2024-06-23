5cecd30f4d414ca151d7b021c10c3337ac3fb5dd
Durgesh Khade <durgeshkhade001@gmail.com>
2024-06-24 03:08:51 +0530

    ok

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log('Server-side code running');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
