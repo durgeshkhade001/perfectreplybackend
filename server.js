a045b171aaed740a4fe6f11cf3f25e4ee2ec2c61
Durgesh Khade <durgeshkhade001@gmail.com>
2024-07-28 21:55:56 +0530

    ..

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 console.clear();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const http = require("http"); // Import http module
+const { Server } = require("socket.io"); // Import socket.io
 
 dotenv.config();
 
@@ -11,12 +13,29 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
