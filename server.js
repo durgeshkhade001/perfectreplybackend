4a888ba802e30df89a8f71b156c28536b7ef210f
Durgesh Khade <durgeshkhade001@gmail.com>
2024-07-29 12:52:53 +0530

    init

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ mongoose.connect(dbURI)
   .then(() => {
     console.log('Connected to MongoDB');
     server.listen(3000, () => {
-      console.log("Server is running on port 3000 \n---------------------------------");
+      console.log("Server is running on port 3000\n" + '-'.repeat(30));
     });
   })
   .catch(err => {
     console.log(err);
-    process.exit(1); // Exit the process with failure
+    process.exit(1);
   });
 
 app.get("/", (req, res) => {
