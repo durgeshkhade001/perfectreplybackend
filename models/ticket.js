fb7283df05ab4d1a6dcd1ab3908f72068cb9e03d
Durgesh Khade <durgeshkhade001@gmail.com>
2024-07-31 23:54:48 +0530

    Refactor ticket.js model for improved ticket management

--- models/ticket.js
+++ models/ticket.js
@@ -2,6 +2,45 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ticketSchema = new Schema({
+    ticketType: {
+        type: Schema.Types.ObjectId,
+        ref: "TicketType",
+        required: true,
+    },
+    collecteddata: {
+        type: Array,
+        default: [],
+    },
+    stage: {
+        type: String,
+        default: "created",
+    },
+    priority: {
+        type: Boolean,
+        default: false,
+    },
+    open: {
+        type: Boolean,
+        default: true,
+    },
+    team: {
+        type: Schema.Types.ObjectId,
+        ref: "Team",
+        default: null,
+    },
+    assignee: {
+        type: Schema.Types.ObjectId,
+        ref: "Agent",
+        default: null,
+    },
+    mentions: {
+        type: Array,
+        default: [],
+    },
+    thread: {
+        type: Array,
+        default: [],
+    },
 }, { timestamps: true });
 
 const Ticket = mongoose.model("Ticket", ticketSchema);
