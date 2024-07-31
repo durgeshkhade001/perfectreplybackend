d9d7800e89819549766349c19c5dc66d478465c9
Durgesh Khade <durgeshkhade001@gmail.com>
2024-07-31 21:22:22 +0530

    feat: Update socketManager.js for live client connections
    This commit adds functionality to the socketManager.js file to handle live client connections. It introduces a new Map called `liveclients` to store the connected clients. When a client connects, their ID is stored in the map along with their socket. The `conn` event is used to establish a connection with a client, and the `islive` event is used to check if a client is still connected. The `disconnect` event is used to remove a client from the `liveclients` map when they disconnect. This update enhances the socket management capabilities of the application.

--- utils/socketManager.js
+++ utils/socketManager.js
@@ -1,8 +1,45 @@
 let io;
+const liveclients = new Map();
 
 const init = (httpServer) => {
   const { Server } = require("socket.io");
   io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    let clientId;
+
+    const connectTimeout = setTimeout(() => {
+      if (!clientId) {
+        socket.disconnect();
+      }
+    }, 5000);
+
+    socket.on("conn", (id) => {
+      if (typeof id !== 'string' || id.trim() === '') {
+        socket.emit("error", "Invalid client ID");
+        return;
+      }
+
+      if (!clientId) {
+        console.log("Client connected with id: " + id);
+        clientId = id;
+        liveclients.set(id, socket);
+        io.emit("islive_" + clientId, true);
+        clearTimeout(connectTimeout);
+      }
+    });
+
+    socket.on("islive", (id) => {
+      socket.emit("islive_" + id, liveclients.has(id));
+    });
+
+    socket.on("disconnect", () => {
+      if (clientId) {
+        liveclients.delete(clientId);
+        io.emit("islive_" + clientId, false);
+      }
+    });
+  });
 };
 
 const emitEvent = (event, data) => {
